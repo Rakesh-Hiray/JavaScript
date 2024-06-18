400010612110ad77230e6235b67b0e05a3c89f2e
Rakesh <hirayrb2015@gmail.com>
2024-06-18 16:18:01 +0530

    Explained: Hoisting in JS

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,56 @@
+// This file demonstrates the concept of the Javascript -> Hoisting
+
+// Hoisting: We can access the Variables & Functions before initializing it.
+
+// ********** Normal code **********
+
+let x = 7;
+
+function getHost() {
+    console.log("In Host");
+}
+
+getHost();  // In Host
+console.log(x); // 7
+
+// ********** Hoisting **********
+
+getHost();  // In Host, allcoated memory with the copy of function
+console.log(y); // undefined, memory is allocated and assigned placeholder 'undefined'
+
+let y = 7;
+
+function getHost() {
+    console.log("In Host");
+}
+
+// ********** Hoisting2 **********
+
+getHost();  // In Host, allcoated memory with the copy of function
+console.log(y); // y is not defined.
+
+function getHost() {
+    console.log("In Host");
+}
+
+// ********** Function **********
+
+getHost();  // undfined
+console.log(y); // undefined, memory is allocated and assigned placeholder 'undefined'
+
+let y = 7;
+
+let getHost = function getHost() {
+    console.log("In Host");
+}
+
+// ********** Arrow Function **********
+
+getHost();  // undfined
+console.log(y); // undefined, memory is allocated and assigned placeholder 'undefined'
+
+let y = 7;
+
+let getHost = () =>  {
+    console.log("In Host");
+}
